3dbab6f2ec36df2e3025729c5f059eb82ad1e372
Thomas Strauszer <tstrauszer@uc.cl>
2022-09-08 14:42:28 -0400

    added alpha_n, beta_n function for computation

--- src/FiniteRankOperator.jl
+++ src/FiniteRankOperator.jl
@@ -1,3 +1,18 @@
+""" returns αₙ and βₙ for a given n"""
+
+function seriesconstant(P::Problem{2,1}, n::Int)
+    αₙ = P.dir[1]*P.pde[1].k + 2π*n/P.L
+    βₙ = sqrt(Complex(P.pde[1].k^2-αₙ^2))
+    return αₙ, βₙ
+end
+function seriesconstant(P::Problem{3,2}, n::Tuple{Int,Int})
+    αₙ₁ = P.dir[1]*P.pde[1].k + 2π*n[1]/P.L[1]
+    αₙ₂ = P.dir[2]*P.pde[1].k + 2π*n[2]/P.L[2]
+    βₙ  = sqrt(complex(P.pde[1].k^2-αₙ₁^2-αₙ₂^2))
+    return αₙ₁, αₙ₂, βₙ
+end
+
+
 """
     Field corrections are computed
 """
@@ -5,10 +20,8 @@
 function fixdelta(P::Problem{N,1}, tol::Float64) where N
     δ = Int[]
     nC= 10
-    α = P.dir[1]*P.pde[1].k
     for n = -nC:nC
-        αₙ = α + 2π*n/P.L
-        βₙ = sqrt(Complex(P.pde[1].k^2-αₙ^2))
+        _,βₙ = seriesconstant(P,n)
         if abs(βₙ) < tol push!(δ,n) end
     end
     return δ
@@ -17,11 +30,8 @@ end
 function fixdelta(P::Problem{3,2}, tol::Float64)
     δ = Tuple{Int,Int}[]
     nC = 10
-    α₁,α₂ = P.dir[1]*P.pde[1].k, P.dir[2]*P.pde[1].k
     for n1 = -nC:nC, n2 = -nC:nC
-        αₙ₁ = α₁ + 2π*n1/P.L[1]
-        αₙ₂ = α₂ + 2π*n2/P.L[2]
-        βₙ = sqrt(Complex(P.pde[1].k^2-αₙ₁^2-αₙ₂^2))
+        _,_,βₙ = seriesconstant(P,(n1,n2))
         if abs(βₙ) < tol push!(δ,(n1,n2)) end
     end
     return δ
@@ -40,8 +50,7 @@ function finiterankoperator(P::Problem{2,1},G::Vector{NystromMesh{N,T,M,NM}},Gt:
     len = 2*length(G[1].dofs)+2*length(G[2].dofs)
     FRO = zeros(ComplexF64,len,len)
     for n in Cδ
-        αₙ = P.dir[1]*P.pde[1].k + 2π*n/P.L
-        βₙ = sqrt(Complex(P.pde[1].k^2-αₙ^2))
+        _,βₙ = seriesconstant(P,n)
 
         Ψₙ⁺ = Ψₙ(P,G,r₁,n₁; n=n, sgn = +1.)
         Ψₙ⁻ = Ψₙ(P,G,r₁,n₁; n=n, sgn = -1.)
@@ -70,9 +79,7 @@ function finiterankoperator(P::Problem{3,2},G::Vector{NystromMesh{N,T,M,NM}},Gt:
     len = 2*length(G[1].dofs)+2*length(G[2].dofs)+2*length(G[4].dofs)
     FRO = zeros(ComplexF64,len,len)
     for n in Cδ
-        αₙ₁ = P.dir[1]*P.pde[1].k + 2π*n[1]/P.L[1]
-        αₙ₂ = P.dir[2]*P.pde[1].k + 2π*n[2]/P.L[2]
-        βₙ  = sqrt(complex(P.pde[1].k^2-αₙ₁^2-αₙ₂^2))
+        _,_,βₙ = seriesconstant(P,(n1,n2))
 
         Ψₙ⁺ = Ψₙ(P,G,r₁,n₁; n=n, sgn = +1.)
         Ψₙ⁻ = Ψₙ(P,G,r₁,n₁; n=n, sgn = -1.)
@@ -81,7 +88,8 @@ function finiterankoperator(P::Problem{3,2},G::Vector{NystromMesh{N,T,M,NM}},Gt:
 
         if abs(βₙ) < 1e-8
             @info "Near RW anomaly" βₙ n
-            error("not implemented")
+            # error("not implemented")
+            @warn "not implemented"
         else
             FRO += exp(im*βₙ*H)/(2*im*βₙ) * (Ψₙ⁻ * Lₙ⁻ - Ψₙ⁺ * Lₙ⁺)
         end
@@ -130,9 +138,10 @@ function scatcorrection(P::Problem{3,2},G::Vector,eval,σw::Vector{ComplexF64}; 
     dR⁻, R⁻, x⁻ = planegradscat(P,G; H = -H)
     fix = zeros(length(eval))
     for n in Cδ
-        αₙ₁ = P.dir[1]*P.pde[1].k + 2π*n[1]/P.L[1]
-        αₙ₂ = P.dir[2]*P.pde[1].k + 2π*n[2]/P.L[2]
-        βₙ  = sqrt(complex(P.pde[1].k^2-αₙ₁^2-αₙ₂^2))
+        # αₙ₁ = P.dir[1]*P.pde[1].k + 2π*n[1]/P.L[1]
+        # αₙ₂ = P.dir[2]*P.pde[1].k + 2π*n[2]/P.L[2]
+        # βₙ  = sqrt(complex(P.pde[1].k^2-αₙ₁^2-αₙ₂^2))
+        αₙ₁,αₙ₂,βₙ = seriesconstant(P,(n1,n2))
         uₙ⁺ = [exp.(im*αₙ₁*r[1] + im*αₙ₂*r[2] + im*βₙ*r[3]) for r in eval]
         uₙ⁻ = [exp.(im*αₙ₂*r[1] + im*αₙ₂*r[2] - im*βₙ*r[3]) for r in eval]
         if abs(βₙ) < 1e-8
@@ -168,46 +177,39 @@ end
 
 function Lₙ(P::Problem{2,1},x, R::Matrix{ComplexF64},dR::Matrix{ComplexF64}; n::Int, sgn) 
     # to integrate you must multiply by Lₙ
-    αₙ = P.dir[1]*P.pde[1].k + 2π*n/P.L
-    βₙ = sqrt(Complex(P.pde[1].k^2-αₙ^2))
+    αₙ,βₙ = seriesconstant(P,n)
     exp.(im*αₙ*x)' * (dR - sign(sgn) * im*βₙ* R) * P.L/length(x) /P.L
 end
 function Lₙ(P::Problem{3,2}, x, R::Matrix{ComplexF64},dR::Matrix{ComplexF64}; n::Tuple{Int64, Int64}, sgn)
-    αₙ₁ = P.dir[1]*P.pde[1].k + 2π*n[1]/P.L[1]
-    αₙ₂ = P.dir[2]*P.pde[1].k + 2π*n[2]/P.L[2]
-    βₙ  = sqrt(complex(P.pde[1].k^2-αₙ₁^2-αₙ₂^2))
+    _,_,βₙ = seriesconstant(P,(n1,n2))
     x1  = [q[1] for q in x]
     x2  = [q[2] for q in x]
-    exp.(im*αₙ₁*x1-im*αₙ₂*x2)' * (dR - sign(sgn)*im*βₙ*R) * P.L[1]*P.L[2]/prod(size(x)) /P.L[1]/P.L[2]
+    exp.(im*αₙ₁*x1+im*αₙ₂*x2)' * (dR - sign(sgn)*im*βₙ*R) * P.L[1]*P.L[2]/prod(size(x)) /P.L[1]/P.L[2]
 end
 
 function dLₙ(P::Problem{2,1},x,R::Matrix{ComplexF64}; n::Int, sgn)
-    αₙ = P.dir[1]*P.pde[1].k + 2π*n/P.L
-    # βₙ = sqrt(Complex(P.pde[1].k^2-αₙ^2))
+    αₙ,_ = seriesconstant(P,n)
     exp.(im*αₙ*x)' * (-im*sign(sgn)*R) * P.L/length(x) /P.L
 end
 
 """ Compute Ψₙ and dΨₙ as a column vector """
 
 function Ψₙ(P::Problem{2,1},G::Vector,r1,n1; n::Int, sgn)
-    αₙ = P.dir[1]*P.pde[1].k + 2π*n/P.L
-    βₙ = sqrt(Complex(P.pde[1].k^2-αₙ^2))
+    αₙ,βₙ = seriesconstant(P,n)
     uₙ = [exp(im*(αₙ*r[1] - sign(sgn)*βₙ*r[2])) for r in r1] # uₙ|Γ₁
     duₙ = im*[(αₙ*ν[1] - sign(sgn)*βₙ*ν[2]) for ν in n1] .* uₙ # duₙ|Γ₁
     [uₙ; duₙ; zeros(2*length(G[2].dofs))]
 end
 
 function Ψₙ(P::Problem{3,2},G::Vector,r1,n1; n::Tuple{Int,Int}, sgn)
-    αₙ₁ = P.dir[1]*P.pde[1].k + 2π*n[1]/P.L[1]
-    αₙ₂ = P.dir[2]*P.pde[1].k + 2π*n[2]/P.L[2]
-    βₙ  = sqrt(complex(P.pde[1].k^2-αₙ₁^2-αₙ₂^2))
+    αₙ₁,αₙ₂,βₙ = seriesconstant(P,(n1,n2))
     uₙ = [exp(im*(αₙ₁*r[1]+αₙ₂*r[2] - sign(sgn)*βₙ*r[3])) for r in r1] # uₙ|Γ₁
     duₙ = im*[(αₙ₁*ν[1]+αₙ₂*ν[2] - sign(sgn)*βₙ*ν[3]) for ν in n1] .* uₙ # duₙ|Γ₁
     [uₙ; duₙ; zeros(2*length(G[2].dofs)+2*length(G[4].dofs))]
 end
 
 function dΨₙ(P::Problem{2,1},G::Vector,r1,n1; n::Int, sgn)
-    αₙ = P.dir[1]*P.pde[1].k + 2π*n/P.L
+    αₙ,_ = seriesconstant(P,n)
     uₙ = [-sign(sgn) * im*r[2] * exp(im*αₙ*r[1]) for r in r1] # uₙ|Γ₁
     vₙ = [-sign(sgn) * (-r1[j][2]*αₙ*n1[j][1] + im*n1[j][2]) * exp(im*αₙ*r1[j][1]) for j=1:length(r1)] # vₙ|Γ₁ #RE-DO
     [uₙ; vₙ; zeros(2*length(G[2].dofs))]
