704fb68ed007846a0028aa736b8a8c716b81c7d8
Thomas Strauszer <tstrauszer@uc.cl>
2022-09-20 13:49:33 -0300

    meshconvergence with different window sizes

--- meshconvergence.jl
+++ meshconvergence.jl
@@ -5,7 +5,7 @@ using PeriodicMedia
 import Plots
 
 
-function meshconvergence(P::Problem,wgf::Window, meshsizes, dim)
+function meshconvergence(P::Problem,WGF::Window, meshsizes, dim)
 
     Shape = PeriodicMedia.ParametricSurfaces.Sphere
     Fig = Obstacle(Shape,minimum(P.L)/4)
@@ -16,7 +16,7 @@ function meshconvergence(P::Problem,wgf::Window, meshsizes, dim)
         Γs = unitcell(P,Fig, WGF; ppw = ppw, dimorder = dim)
         Γt = extendedcell(P,Fig, WGF; ppw = ppw, dimorder = dim)
         ϕ  = solver(P,Γs,Γt,WGF; FRO = false)
-        @show eb = energytest(P,Γt,WGF, ϕ; FRO = false, H = wgf.c*wgf.A)
+        @show eb = energytest(P,Γt,WGF, ϕ; FRO = false, H = WGF.c*WGF.A)
         push!(err,eb)
     end
     return err
@@ -24,7 +24,7 @@ end
 
 
 setup = "3D2D"
-p = Vector{Plots.Plot}(undef,3)
+# p = Vector{Plots.Plot}(undef,3)
 
 if setup == "3D2D"
     # k1 = [9.0, 9.199221756451442, 9.5]
@@ -34,14 +34,18 @@ if setup == "3D2D"
     
     ####################################
     P = Problem([k1[1],15.], θ, L; ambdim = 3, geodim = 2)
-    meshsizes = collect(3:5)
-    dim       = 2
-    WGF       = Window(0.2, 15*(2π/k1[1]))
+    meshsizes = collect(1:3)
+    dim       = 1
+    wdwsizes  = collect(5:5:15) 
     ####################################
 
-    e = meshconvergence(P,WGF,meshsizes,dim)
-    p = Plots.plot(meshsizes, log10.(e), ylims = (-4.5,-0.5))
-
+    p = Plots.plot()
+    λ = (2π/k1[1])
+    for w in wdwsizes
+        WGF = Window(0.3, w*λ)
+        e   = meshconvergence(P,WGF,meshsizes,dim)
+        p   = Plots.plot!(meshsizes, log10.(e), ylims = (-4.5,-0.5), label = "A/λ="*string(WGF.A/λ))
+    end
     Plots.savefig(p, "meshconv.png") 
 end
 
