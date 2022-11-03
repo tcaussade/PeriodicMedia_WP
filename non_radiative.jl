9a390dd3458b677367dfb518376a1207c7843c05
Thomas Strauszer <tstrauszer@uc.cl>
2022-11-03 12:44:24 -0300

    nonradiative update experiment

--- non_radiative.jl
+++ non_radiative.jl
@@ -21,17 +21,55 @@ function nonradiative(P,G,σw; h)
     # dR⁻, R⁻, x⁻ = PeriodicMedia.linegradscat(P,G; H = -h)
     C⁺ = []
     C⁻ = []
+    B⁺ = []
+    B⁻ = []
     betas = []
     for n ∈ Cδ
         _,_,βₙ = PeriodicMedia.seriesconstant(P,(n[1],n[2]))
-        # _,βₙ = PeriodicMedia.seriesconstant(P,n)
-        Cₙ⁺ = +exp(im*βₙ*h)/(2*im*βₙ) * PeriodicMedia.Lₙ(P,x⁺,R⁺,dR⁺; n=n, sgn = +1.0) * σw
-        Cₙ⁻ = -exp(im*βₙ*h)/(2*im*βₙ) * PeriodicMedia.Lₙ(P,x⁻,R⁻,dR⁻; n=n, sgn = -1.0) * σw
+
+        Lₙ⁺ = PeriodicMedia.Lₙ(P,x⁺,R⁺,dR⁺; n=n, sgn = +1.0) * σw
+        Lₙ⁻ = PeriodicMedia.Lₙ(P,x⁻,R⁻,dR⁻; n=n, sgn = -1.0) * σw
+
+        Cₙ⁺ = -exp(im*βₙ*h)/(2*im*βₙ) * Lₙ⁺ 
+        Cₙ⁻ = +exp(im*βₙ*h)/(2*im*βₙ) * Lₙ⁻ 
         push!(C⁺,Cₙ⁺)
         push!(C⁻,Cₙ⁻)
+
+        Bₙ⁺ = +exp(-im*βₙ*h)/(2*im*βₙ) * Lₙ⁻ 
+        Bₙ⁻ = -exp(-im*βₙ*h)/(2*im*βₙ) * Lₙ⁺ 
+        push!(B⁺,Bₙ⁺)
+        push!(B⁻,Bₙ⁻)
+
         push!(betas,βₙ)
     end
-    return betas, C⁺,C⁻
+    return betas, C⁺,C⁻, B⁺,B⁻
+end
+function etest(bp,bm)
+    α₁,α₂,β = P.pde[1].k*P.dir
+    S = zero(ComplexF64)
+    R = zero(ComplexF64)
+    for (i,(n1,n2)) in enumerate(Cδ)
+        αₙ₁ = α₁ + n1*2π/P.L[1]
+        αₙ₂ = α₂ + n2*2π/P.L[2]
+        βₙ = sqrt(complex(P.pde[1].k^2 - αₙ₁^2 - αₙ₂^2 ))
+        if P.pde[1].k^2 ≥ abs(αₙ₁)^2 + abs(αₙ₂)^2
+            βₙ = sqrt(complex(P.pde[1].k^2 - αₙ₁^2 - αₙ₂^2 ))
+            # @show B⁺ = exp(-im*βₙ*h)/P.L[1]/P.L[2] * sum( u1.*exp.(-im*αₙ₁*xi-im*αₙ₂*yi) ) *P.L[1]*P.L[2]/length(u1)
+            # @show B⁻ = exp(-im*βₙ*h)/P.L[1]/P.L[2] * sum( u2.*exp.(-im*αₙ₁*xi-im*αₙ₂*yi) ) *P.L[1]*P.L[2]/length(u1)
+            B⁺ = bp[i]
+            B⁻ = bm[i]
+            if n1 == 0 && n2 == 0
+                global B₀⁻ = B⁻
+            end
+            S += βₙ/β * (abs(B⁺)^2+abs(B⁻)^2)
+            R += βₙ/β * abs(B⁺)^2
+            # T += βₙ/β * abs(B⁻)^2
+        end
+    end
+    e = abs(2*real(B₀⁻)+S)
+    # T = 1 + 2*real(B₀⁻) + S - R
+    # abs(2*real(B₀)+S)
+    return e
 end
 function run_experiment(P,Wpar)
     
@@ -46,7 +84,7 @@ function run_experiment(P,Wpar)
     b       = rightside(P,Γs)
     ϕ       = solver(E,MB,Wa,b)
 
-    betas, C⁺,C⁻ = nonradiative(P,Γt,Wa*ϕ; h = he)
+    nonradiative(P,Γt,Wa*ϕ; h = he)
 end
 
 PeriodicMedia.clear_entities!()
@@ -67,8 +105,8 @@ Shape = PeriodicMedia.ParametricSurfaces.Sphere
 Fig = Obstacle(Shape,minimum(L)/4)
 
 
-global ppw = 3
-global dim = 2
+global ppw = 1
+global dim = 1
 
 # correction parameters
        δ    = 1.5*k1
@@ -80,7 +118,10 @@ global Cδ = PeriodicMedia.fixdelta(P,δ)
 Asizes = collect(3:1:5)
 cp  = Matrix{Float64}(undef,length(Cδ),length(Asizes))
 cm  = Matrix{Float64}(undef,length(Cδ),length(Asizes))
+e   = Vector{Float64}(undef,length(Asizes))
 betas = Vector{ComplexF64}(undef,length(Cδ))
+
+name_exp = "non_radiative"*"_ppw"*string(ppw)*"_dim"*string(dim)*"_k"*string(k1)*string(".txt")
 
 for (i,Ap) in enumerate(Asizes)
     # set discretization parameters
@@ -90,7 +131,9 @@ for (i,Ap) in enumerate(Asizes)
     A    = Ap * λ
     Wpar = Window(c,A)
 
-    betas, c⁺,c⁻ = run_experiment(P,Wpar)
+    betas, c⁺,c⁻,b⁺,b⁻ = run_experiment(P,Wpar)
+
+    @show e[i]    = etest(b⁺,b⁻)
     cp[:,i] .= abs.(c⁺)
     cm[:,i] .= abs.(c⁻)
 end
